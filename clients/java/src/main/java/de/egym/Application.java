ea8753c3af3f2da27c78648aae46d4aab7a6fc99
Shawn Davies <shawn.davies@egym.de>
2017-10-05 16:23:18 +0200

    Initial commit of the basic java client

---
+++ clients/java/src/main/java/de/egym/Application.java
@@ -0,0 +1,42 @@
+/**
+ * This file is part of the source code and related artifacts for eGym Application.
+ *
+ * Copyright © 2017 eGym GmbH
+ */
+package de.egym;
+
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public final class Application {
+
+	private static final String HOST = "tcp://35.187.37.87:5556";
+
+	private Application() {
+
+	}
+
+	private void subscribeAndAwait() {
+		try (final ZContext context = new ZContext()) {
+			final ZMQ.Socket socket = context.createSocket(ZMQ.SUB);
+
+			if (socket.connect(HOST) && socket.subscribe("")) {
+				processEvents(socket);
+			}
+		}
+	}
+
+	private void processEvents(final ZMQ.Socket socket) {
+		while (true) {
+			final String[] data = socket.recvStr().split(" ", 2);
+			final String topic = data[0];
+			final String payload = data[1];
+			System.out.println(topic + ", " + payload);
+		}
+	}
+
+	public static void main(String[] args) {
+		final Application application = new Application();
+		application.subscribeAndAwait();
+	}
+}
