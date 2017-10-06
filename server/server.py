663909a03f864fce30c275ba4cbe05f12c4c5b39
Pedro Rosa <pedro.rosa@egym.de>
2017-10-06 23:55:09 +0200

    fix machine messages

--- server/server.py
+++ server/server.py
@@ -2,23 +2,33 @@
 import zmq
 from time import sleep
 from google.cloud import pubsub_v1
+import json
 
 context = zmq.Context()
-consumer = context.socket(zmq.PULL)
-consumer.bind('tcp://*:5557')
-
 publisher = context.socket(zmq.PUB)
 publisher.bind("tcp://*:5556")
 print("zeroMQ server started")
 
+def createMessage(message):
+    decoded_message = message.data.decode("utf-8")
+    payload = "null" if decoded_message == "null" else json.loads("{" + decoded_message + "}")
+    return '{} {}'.format(message.attributes['message_type'], json.dumps(
+        {'machine_id': message.attributes['machine_id'],
+         'machine_type': message.attributes['machine_type'],
+         'timestamp': message.attributes['timestamp'],
+         'rfid': message.attributes['rfid'],
+         'payload': payload}))
 
 def listen(project, subscription_name):
     subscriber = pubsub_v1.SubscriberClient()
     subscription_path = subscriber.subscription_path(project, subscription_name)
 
     def callback(message):
-        publisher.send_string('{} {}'.format(message.attributes['message_type'], message.data))
-        message.ack()
+        try:
+            publisher.send_string(createMessage(message))
+            message.ack()
+        except json.decoder.JSONDecodeError as e:
+            pass
 
     subscriber.subscribe(subscription_path, callback=callback)
 
