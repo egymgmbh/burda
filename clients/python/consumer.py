3d04460159c47effe27283bf04dfdfd45cc9d217
Murray Cumming <murray.cumming@egym.de>
2017-10-18 09:28:22 +0200

    python client: Adapt to message_type as a value.

--- clients/python/consumer.py
+++ clients/python/consumer.py
@@ -12,13 +12,25 @@ def subscribe_with_filter(host, topic_filter):
     subscriber.connect(host)
     return subscriber
 
-def await_and_consume(subscriber):
+def await_and_consume(subscriber, handlers):
     while True:
         msg = subscriber.recv()
         try:
             obj = json.loads(msg)
-            print(obj)
+            # print(obj)
+
+            message_type = obj['message_type']
+            handler = handlers.get(message_type, None)
+            if handler:
+                handler(obj['body'])
+            else:
+                print('No handler for message type: ' + message_type)
+
         except ValueError:
             print('Failed to parse JSON: ' + msg)
 
-await_and_consume(subscribe("tcp://35.189.246.57:5556"))
+def login_topic_handler(data):
+        print("Login with data: {}".format(data))
+
+
+await_and_consume(subscribe("tcp://35.189.246.57:5556"), {'login': login_topic_handler})
